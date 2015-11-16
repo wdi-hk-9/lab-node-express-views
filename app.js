c3c39f0a5d51e08287d834e1c30f46f625d36798
franxyzxyz <franxyzxyxz@gmail.com>
2015-11-16 23:44:34 +0800

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var logger = require('morgan');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
+var connect        = require('connect');
+var methodOverride = require('method-override');
 
 // database setup
 var mongoose = require('mongoose');
@@ -12,6 +15,13 @@ mongoose.connect('mongodb://localhost:27017/express-views-and-quotes');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 app.use(express.static(__dirname + '/public'));
 
