59cb6d21664179e4f98e9af6d0d12488d17fa5fb
adaau <ada.au.cheng@gmail.com>
2015-11-16 20:59:59 +0800

    initial commit

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
-var express = require('express'),
-    router = express.Router(),
-    bodyParser = require('body-parser'), //parses information from POST
-    methodOverride = require('method-override'); //used to manipulate POST
+var express         = require('express'),
+    router          = express.Router(),
+    bodyParser      = require('body-parser'), //parses information from POST
+    methodOverride  = require('method-override'); //used to manipulate POST
 
 var quotesController = require('../controllers/quotesController');
 
