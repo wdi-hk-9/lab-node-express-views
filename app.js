dfaf004006702f60a8f0a3f115a57357293809f1
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-17 08:17:32 +0800

    home page working

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var expressLayouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
 
 // database setup
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/express-views-and-quotes');
 
+//template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+
 // app setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
 
 // our routes
 var routes = require('./config/routes');
--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -1,13 +1,16 @@
 var Quote = require('../models/Quote');
 
-
 // INDEX
 function getAll(request, response) {
   Quote.find(function(error, quotes) {
     if(error) response.json({message: 'Could not find any quote'});
-    response.render('layout', {quotes: quotes});
+    response.render('partials/index', {quotes: quotes});
     // response.json({quotes: quotes});
   });
+}
+
+function newQuote(request, response){
+  response.render('partials/new')
 }
 
 // CREATE
@@ -19,7 +22,7 @@ function createQuote(request, response) {
   quote.save(function(error) {
     if(error) response.json({messsage: 'Could not ceate quote b/c:' + error});
     console.log(quote);
-    response.render('partials/quote/params.id', {quote :quote})
+    response.render('partials/index', {quote :quote})
     // response.json(quote);
   });
 }
@@ -30,8 +33,8 @@ function getQuote(request, response) {
 
   Quote.findById({_id: id}, function(error, quote) {
     if(error) response.json({message: 'Could not find quote b/c:' + error});
-    response.render('partials/candies/edit', {quote: quote})
-    // response.json({quote: quote});
+    // response.render('partials/edit', {quote: quote})
+    response.json({quote: quote});
   });
 }
 
@@ -66,6 +69,7 @@ function removeQuote(request, response) {
 
 module.exports = {
   getAll: getAll,
+  newQuote: newQuote,
   createQuote: createQuote,
   getQuote: getQuote,
   updateQuote: updateQuote,
